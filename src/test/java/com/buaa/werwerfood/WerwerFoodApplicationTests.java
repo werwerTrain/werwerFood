54fea99d09cce0ad9be1bd53fa17ce5c0924fdcf
WZQ <13875445+bush04@user.noreply.gitee.com>
2024-08-24 09:15:36 +0800

    init

---
+++ src/test/java/com/buaa/werwerfood/WerwerFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buaa.werwerfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerwerFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
