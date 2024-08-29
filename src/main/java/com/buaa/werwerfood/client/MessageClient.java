a1a148f8333259a14edab2d6344112a135c1d6e1
WZQ <13875445+bush04@user.noreply.gitee.com>
2024-08-29 14:16:45 +0800

    微服务降级

--- src/main/java/com/buaa/werwerfood/client/MessageClient.java
+++ src/main/java/com/buaa/werwerfood/client/MessageClient.java
@@ -1,12 +1,14 @@
 package com.buaa.werwerfood.client;
 
+import com.buaa.werwerfood.client.fallback.MessageClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 
-@FeignClient(name = "user-service",contextId = "messageClient", path = "/api/users")
+@FeignClient(name = "user-service",contextId = "messageClient", path = "/api/users",
+fallback = MessageClientFallback.class)
 public interface MessageClient {
     @PostMapping("/addMessage")
     void addMessage(@RequestBody Map<String, Object> messageMap);
---
+++ src/main/java/com/buaa/werwerfood/client/fallback/TrainClientFallback.java
@@ -0,0 +1,22 @@
+package com.buaa.werwerfood.client.fallback;
+
+import com.buaa.werwerfood.DTO.TrainDTO;
+import com.buaa.werwerfood.DTO.TrainOrderDTO;
+import com.buaa.werwerfood.client.TrainClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainClientFallback implements TrainClient {
+    @Override
+    public List<TrainOrderDTO> getTrainOrderByTrainAndIdentification(String tid, String date, String userID) {
+        System.out.println("Get trainOrder by train and id request failed, fallback method executed.");
+        return new ArrayList<>();
+    }
+
+    @Override
+    public TrainDTO getTrainByTidAndDate(String tid, String date) {
+        System.out.println("Get train by tid and date request failed, fallback method executed.");
+        return new TrainDTO();
+    }
+}
